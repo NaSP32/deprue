5576cefcbf5f12cc689cbe23f2b4dde0c5a0ca69
Nadia Soledad Pereyra <nadiapereyra32@gmail.com>
2024-07-11 13:50:43 -0300

    Ejercicios de funciones con condicionales

--- app.js
+++ app.js
@@ -103,4 +103,90 @@ function multiplicarPorSiMismo(unNumero){
 }
 let multiplicacion = multiplicarPorSiMismo(10980);
 console.log(multiplicacion);
+/*
+Crea una función que calcule el índice de masa corporal 
+(IMC) de una persona a partir de su altura en metros y 
+peso en kilogramos, que se recibirán como parámetros.
+*/
 
+function indiceMasaCorporal(altura, peso) {
+    let imc = peso / altura;
+    return(imc); 
+
+}
+indiceMasaCorporal(1.59, 68);
+console.log(indiceMasaCorporal);
+
+/*
+Crea una función que calcule el valor del factorial 
+de un número pasado como parámetro.
+ */
+function calcularFactorial(numero) {
+    if (numero === 0 || numero === 1) {
+      return 1;
+    } else {
+      return numero * calcularFactorial(numero - 1);
+    }
+  }
+  // Ejemplo de uso
+  let numero = 4;
+  let resultado = calcularFactorial(numero);
+  console.log(`El factorial de ${numero} es ${resultado}`);
+
+/*
+ Crea una función que convierta un valor en dólares, pasado 
+ como parámetro, y devuelva el valor equivalente en reales
+ (moneda brasileña,si deseas puedes hacerlo con el valor del dólar
+  en tu país). Para esto, considera la cotización del dólar igual 
+  a R$4,80.
+ */
+
+function conversion(dolares) {
+    let ARS = 1435;
+    let convertidos = 0;
+    convertidos = (dolares * ARS);
+    return convertidos;
+}
+let valorEnDolares = 190;
+let valorEnPesos = conversion(190);
+console.log(`${valorEnDolares} dolares son ${valorEnPesos} ARS`);
+
+/*
+Crea una función que muestre en pantalla el área y el perímetro 
+de una sala rectangular, utilizando la altura y la anchura que 
+se proporcionarán como parámetros.
+ */
+
+function areaSala(altura, anchura){
+    area = (altura * anchura);
+    perimetro = (altura + anchura)*2;
+}
+areaSala(35, 4);
+console.log(`El area de la sala es ${area} m2 y el perímetro es ${perimetro} m.`);
+
+/*
+Crea una función que muestre en pantalla el área y el perímetro 
+de una sala circular, utilizando su radio que se proporcionará 
+como parámetro. Considera Pi = 3,14.
+*/
+function areaCircular(radio){
+    areaCirculo = (3.14 * (radio*radio));
+    perimetroCirculo = (3.14 * (radio * 2));
+}
+areaCircular(8) 
+console.log(areaCirculo, perimetroCirculo);
+
+/*
+Crea una función que muestre en pantalla la tabla de multiplicar 
+de un número dado como parámetro.
+ */
+
+function multiplicar(tabla){
+    let contador = 1;
+    while (contador <= 10){
+        resultadoMulti = tabla * contador;
+        console.log(`${contador} * ${tabla} = ${resultadoMulti}`)
+        contador ++;
+    }
+}
+multiplicar(3);
